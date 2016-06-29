3d680ad6865c43c9a103dd4d304ea016a25a3019
urosjarc <urosjarc@users.noreply.github.com>
2016-06-29 19:09:21 +0200

    Every thing set for testing

--- config/scripts/post_install.sh
+++ config/scripts/post_install.sh
@@ -1,10 +1,15 @@
+echo '\nInstall zsh...\n'
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-
 chsh -s $(which zsh)
+
+echo '\nSet default browser...\n'
 sudo update-alternatives --config x-www-browser
 
-ssh-keygen ... /home/urosjarc/.ssh/id_rsa
-cat /home/urosjarc/.ssh/id_rsa.pub
+echo '\nSet keygen for github:\n - file: /home/<user>/.ssh/id_rsa_github\n - passphrase: SKIP'
+ssh-keygen -t rsa -b 4096
 
-ssh-keygen ... /home/urosjarc/.ssh/id_rsa_bb
-cat /home/urosjarc/.ssh/id_rsa_bb.pub
+echo '\nSet keygen for bitbucket:\n - file: /home/<user>/.ssh/id_rsa_bitbucket\n - passphrase: SKIP'
+ssh-keygen -t rsa -b 4096
+
+echo '\nAdding .MY_login source to .profile file...\n'
+grep -q -F 'source ~/.MY_login' ~/.profile || echo 'source ~/.MY_login' >> ~/.profile
