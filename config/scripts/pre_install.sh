6714db2a3a57b1afb63ee730e605ef8b2e11e5b6
urosjarc <jar.fmf@gmail.com>
2016-12-05 14:50:33 +0100

    Adding interface to android.

--- config/scripts/pre_install.sh
+++ config/scripts/pre_install.sh
@@ -2,9 +2,10 @@ wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' > /tmp/neo4j.list
 sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
 
-sudo add-apt-repository ppa:cwchien/gradle -y
-sudo add-apt-repository ppa:indicator-brightness/ppa -y
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo add-apt-repository ppa:cwchien/gradle              -y
+sudo add-apt-repository ppa:indicator-brightness/ppa    -y
+sudo add-apt-repository ppa:webupd8team/java            -y
+sudo add-apt-repository ppa:nilarimogard/webupd8        -y
+sudo add-apt-repository ppa:phablet-team/tools          -y
 
 sudo apt-get update
