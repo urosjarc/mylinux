1c142a94b241f5d7b3a81f23d6a93e81160a3901
Uroš Jarc <jar.fmf@gmail.com>
2018-12-07 07:36:25 +0100

    Update.

--- layouts/monitor.sh
+++ layouts/monitor.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output VGA-0 --primary --mode 1680x1050 --pos 0x0 --rotate normal --output LVDS-0 --off --output DP-6 --off --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output VGA-0 --primary --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr --output LVDS-0 --off
