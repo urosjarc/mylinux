7e7734c4e3a7b8c82c6b508de925ca701c2797a0
urosjarc <jar.fmf@gmail.com>
2021-03-10 08:16:17 +0100

    Adding some stuff

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 sudo apt install make
-## Setup latest versions of software...
-## Update background images from /usr/share/backgrounds
-## Update ./data/dotfiles/~_|_.i3_|_xrandr.sh
-## Update ./data/dotfiles/~_|_.gitconfig (username, email)
-## Update ./data/dotfiles/~_|_.linux
-## Make bindings help for i3 from ./data/dotfiles/~_|_.i3_|_config
-## Vimium bindings auto setup on chromium data/custom
+
+nautilus /usr/share/backgrounds/ ./data/background/
