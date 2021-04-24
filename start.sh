c20aeb0398d55b574b8bc0d6f1dc8638ae00537e
urosjarc <jar.fmf@gmail.com>
2021-04-24 19:31:48 +0200

    Adding better starting script

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
-sudo apt install make
+printS () { printf "\n\n... $1 ...\n\n"; read -p "Press enter to continue"; }
+open () { xdg-open $1 > output.log 2>&1 & }
 
+printS "Install make"
+sudo apt install -y make
+
+printS "Setup background images"
 nautilus /usr/share/backgrounds/ ./data/background/
+
+printS "Setup apps versions"
+open config/variables 
+open https://nodejs.org 
+open https://www.jetbrains.com/pycharm/download/#section=linux
+open https://www.jetbrains.com/idea/download/#section=linux
+open https://www.jetbrains.com/webstorm/download/#section=linux
+open https://www.jetbrains.com/clion/download/#section=linux 
+open https://github.com/SSNikolaevich/DejaVuSansCode/releases
