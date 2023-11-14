4cb57467787d7a07725e37b25839246d04eb3eec
urosjarc <jar.fmf@gmail.com>
2023-11-14 03:17:19 +0100

    Adding update apt.

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ printS () { printf "\n\n... $1 ...\n\n"; read -p "Press enter to continue"; }
 open () { xdg-open $1 > output.log 2>&1 & }
 
 printS "Install make"
+sudo apt update
 sudo apt install -y make
 
 printS "Setup background images"
@@ -10,5 +11,5 @@ open ./data/background/
 
 
 printS "Setup user info and versions"
-open config/variables 
+open config/variables
 open https://github.com/SSNikolaevich/DejaVuSansCode/releases
